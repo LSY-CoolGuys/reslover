d07ce07a523ebbefacec71429dc11639d9a41b89
issimo1 <2530566719@qq.com>
2024-06-06 16:26:00 +0800

    更新回调接口

--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,16 @@ ADD . /app
 RUN cd /app \
     && go env -w GOPROXY=https://goproxy.cn,direct \
     && go mod tidy\
-    && go build -o reslove .
+    && go build -o resolve .
 
 
 FROM alpine:latest
 WORKDIR /app
 RUN apk update && apk add inotify-tools
-COPY --from=builD /app/reslove /app/resolve
+COPY --from=builD /app/resolve /app/resolve
 COPY --from=builD /app/monitor.sh /app/monitor.sh
 RUN chmod +x /app/resolve
 RUN chmod +x /app/monitor.sh
 RUN cd /app
 
-CMD ["./monitor.sh"]
+CMD ["/bin/sh","-c","./monitor.sh && ./resolve"]
--- mointor.sh
+++ mointor.sh
@@ -4,20 +4,19 @@ WATCH_DIR=${WATCH_DIR}
 JUNIT_FILE="junit.xml"
 OUTPUT_FILE=${OUTPUT_FILE}
 
+
+echo "WATCH_DIR= $WATCH_DIR "
+
+if [ ! -d "$WATCH_DIR" ]; then
+    mkdir -p "$WATCH_DIR"
+    echo "$WATCH_DIR is created"
+fi
+
 echo "Monitoring directory: $WATCH_DIR for JUnit file: $JUNIT_FILE"
 
 inotifywait -m -e create "$WATCH_DIR" | while read path action file; do
     if [ "$file" = "$JUNIT_FILE" ]; then
         echo "Detected $JUNIT_FILE creation. Processing $OUTPUT_FILE."
-
-        if [ -f "$OUTPUT_FILE" ]; then
-          ./resolve
-
-            echo "Processing completed."
-        else
-            echo "Output file $OUTPUT_FILE not found!"
-        fi
-
         exit 0
     fi
 done
