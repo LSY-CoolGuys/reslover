bd206de8d2fdaba3f70b59cf8672d087fddbb22f
issimo1 <2530566719@qq.com>
2024-06-14 11:06:08 +0800

    回调两次--build-ci-cd

--- cmd/load.go
+++ cmd/load.go
@@ -15,55 +15,33 @@ func ParseLoad() {
 		logrus.Infof("解析junit文件错误，%v", err)
 		return
 	}
+	var zero float64 = 0
+	status := 1
+	if createTime == zero && deleteTime == zero {
+		status = 0
+	}
+	overCallBack := map[string]interface{}{
+		"createTime": createTime,
+		"deleteTime": deleteTime,
+	}
 
+	body := map[string]interface{}{
+		"values": overCallBack,
+	}
+	jsonData, err := json.Marshal(body)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	//
+	scale := os.Getenv("LOW_HIGH_SCALE")
 	if os.Getenv("IS_CALL_BACK") == "true" {
-		fs, err := os.ReadFile("/var/load/firstData")
-		if err != nil {
-			logrus.Infof("获取第一次结果错误，%v", err)
-			return
-		}
-		overCallBack := map[string]interface{}{
-			os.Getenv("LOW_SCALE"): string(fs),
-			os.Getenv("HIGH_SCALE"): map[string]float64{
-				"createTime": createTime,
-				"deleteTime": deleteTime,
-			},
-		}
-		body := map[string]interface{}{
-			"values": overCallBack,
-		}
-		jsonData, err := json.Marshal(body)
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
-		//
-		var zero float64 = 0
-		status := 1
-		if createTime == zero && deleteTime == zero {
-			status = 0
-		}
-		callBackUrl := url + fmt.Sprintf("?status=%d", status)
-		// 回调
-		if err = callbackBackend(jsonData, callBackUrl); err != nil {
-			logrus.Infof("回调失败，%v", err)
-			return
-		}
-	} else {
-		callbackInfo := map[string]float64{
-			"createTime": createTime,
-			"deleteTime": deleteTime,
-		}
-		jsonData, err := json.Marshal(callbackInfo)
-		if err != nil {
-			logrus.Infof("序列化第一次测试结果失败，%v", err)
-			return
-		}
-		err = os.WriteFile("/var/load/firstData", jsonData, 0644)
-		if err != nil {
-			logrus.Infof("吸入configmap挂载文件失败，err=%v", err)
-			return
-		}
-		logrus.Infof("成功写入，data=%v", jsonData)
+		scale = os.Getenv("HIGH_SCALE")
+	}
+	callBackUrl := url + fmt.Sprintf("?status=%d&scale=%v", status, scale)
+	// 回调
+	if err = callbackBackend(jsonData, callBackUrl); err != nil {
+		logrus.Infof("回调失败，%v", err)
+		return
 	}
 }
